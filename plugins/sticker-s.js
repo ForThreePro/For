76da5f502cc0091cb9f60585cf0558c35722171c
YALLICO OWNER <yallico2024@gmail.com>
2026-07-03 18:50:08 -0500

    Actualizar sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     let mime = (q.msg || q).mimetype || q.mediaType || ''
     if (!/webp|image|video/g.test(mime)) return m.reply(`Responde a una imagen, video o gif para convertirlo en sticker.`)
     let img = await q.download()
-    let stiker = await sticker(img, false, 'Box Bot MD', 'Barboza')
+    let stiker = await sticker(img, false, 'For Three', 'Whois')
     conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
 }
 handler.help = ['s']
