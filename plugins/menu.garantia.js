efe2e7bee5448205b21c1877112e9a17f30341f6
YALLICO OWNER <yallico2024@gmail.com>
2026-07-04 06:02:01 -0500

    Actualizar menu.garantia.js

--- plugins/menu.garantia.js
+++ plugins/menu.garantia.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn }) => {
   let txt = `*🛡️ [ GARANTÍA FOR THREE ]* 🛡️\n\n`
   txt += `━━━━━━━━━━\n`
   txt += `✅ *Bot 100% Funcional* al comprar\n`
-  txt += `✅ *Soporte 7 Días* por cualquier bug\n`
+  txt += `✅ *Soporte 24/7* por cualquier bug\n`
   txt += `✅ *Actualizaciones* gratis 1 mes\n`
   txt += `✅ *Te enseño a usarlo* por nota\n`
   txt += `━━━━━━━━━━\n\n`
-  txt += `*Si falla en 7 días, te lo arreglo gratis o devolución.*\n\n`
+  txt += `*Si falla en 7 días, te lo arreglo gratis.*\n\n`
   txt += `*¿Lo quieres?*.comprarbot`
   
   await conn.reply(m.chat, txt, m)
